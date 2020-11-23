e59db1f0da2015f006c2571843efc24f56029940
toby cabot <toby@acnodal.io>
2020-11-23 16:13:58 -0500

    Add a ProxyIfindex field to the LoadBalancer status
    We need this because we have to figure out what the proxy pod's
    ifindex is outside of k8s because of namespace issues, but we need to
    use that ifindex inside k8s. We need a way to store the ifindex and
    look it up later.
    
    At the moment the field is populated by a Python daemon running on the
    host, and it's used by the load balancer controller.

--- api/v1/loadbalancer_types.go
+++ api/v1/loadbalancer_types.go
@@ -53,6 +53,12 @@ type LoadBalancerStatus struct {
 	// GUEAddress is the EGW's GUE tunnel endpoint address for this load
 	// balancer.
 	GUEAddress string `json:"gue-address"`
+
+	// ProxyIfindex is the ifindex of the veth interface on the docker
+	// side of this service's proxy pod. In other words, it's the end of
+	// the veth that's inside the container (i.e., on the other side
+	// from the end that's attached to the multus bridge).
+	ProxyIfindex int `json:"proxy-ifindex"`
 }
 
 // +kubebuilder:object:root=true
