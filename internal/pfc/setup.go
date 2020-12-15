2cb311f3c757915f404a15cef2682727d61c1236
toby cabot <toby@acnodal.io>
2020-12-15 11:23:30 -0500

    Fix cli_cfg invocation
    We were passing an extra parameter which cli_cfg wasn't expecting.
    
    We don't need to do the get/grep/set dance, cli_cfg will update if
    appropriate.

--- internal/pfc/setup.go
+++ internal/pfc/setup.go
@@ -50,16 +50,14 @@ func AddQueueDiscipline(nicName string) error {
 // AddFilter adds the pfc filter to the nic if it's not already there.
 func AddFilter(nic string, direction string, filter string) error {
 	script := fmt.Sprintf("/usr/sbin/tc filter show dev %[1]s %[2]s | grep pfc_%[3]s_tc || /usr/sbin/tc filter add dev %[1]s %[2]s bpf direct-action object-file /opt/acnodal/bin/pfc_%[3]s_tc.o sec .text", nic, direction, filter)
+	ctrl.Log.Info("add filter", "script", script)
 	cmd := exec.Command("/bin/sh", "-c", script)
 	return cmd.Run()
 }
 
 func configurePFC(nic string, qid int, flags int, direction string) error {
-	rxtx := map[string]string{"ingress": "rx", "egress": "tx"}[direction]
-	directionID := map[string]int{"ingress": 0, "egress": 1}[direction]
-
-	// configure the PFC only if it hasn't been already
-	script := fmt.Sprintf("/opt/acnodal/bin/cli_cfg get %[1]s | grep -i \"%[4]s *%[1]s\" || /opt/acnodal/bin/cli_cfg set %[1]s %[5]d %[2]d %[3]d \"%[1]s %[6]s\"", nic, qid, flags, direction, directionID, rxtx)
+	// configure the PFC
+	script := fmt.Sprintf("/opt/acnodal/bin/cli_cfg set %[1]s %[2]d %[3]d \"%[1]s %[4]s\"", nic, qid, flags, direction)
 	ctrl.Log.Info("pfc configuration", "script", script)
 	cmd := exec.Command("/bin/sh", "-c", script)
 	return cmd.Run()
