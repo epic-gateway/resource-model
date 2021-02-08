c026c1bd3e1f05f8105bcc123d1c0f40fa50b903
toby cabot <toby@acnodal.io>
2021-02-08 16:52:46 -0500

    At startup, initialize data left over from before
    Some data, like ifindex values, might change during a system reboot,
    so when we're first starting up we can't trust any data that we put
    into CR Statuses. We need to wipe out the LoadBalancer Statuses, but
    that means that we also need to trigger the Python setup-network
    daemon to re-scan the veth and ifindex for each Envoy proxy pod.
    
    This is more ad-hoc that I'd like because the k8s clients that the
    controller-runtime manager uses aren't initialized until the manager
    starts the controllers, but we want to do this work before any
    controller starts.

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
 	"os"
+	"time"
 
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	"k8s.io/client-go/rest"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
 	marin3r "github.com/3scale/marin3r/apis/marin3r/v1alpha1"
@@ -17,6 +24,7 @@ import (
 
 	egwv1 "gitlab.com/acnodal/egw-resource-model/api/v1"
 	"gitlab.com/acnodal/egw-resource-model/controllers"
+	"gitlab.com/acnodal/egw-resource-model/internal/exec"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -24,6 +32,10 @@ var (
 	scheme   = runtime.NewScheme()
 	setupLog = ctrl.Log.WithName("setup")
 )
+
+// +kubebuilder:rbac:groups="",resources=pods,verbs=list;get;watch;update
+// +kubebuilder:rbac:groups=egw.acnodal.io,resources=loadbalancers,verbs=get;list
+// +kubebuilder:rbac:groups=egw.acnodal.io,resources=loadbalancers/status,verbs=get;update
 
 func init() {
 	utilruntime.Must(marin3r.AddToScheme(scheme))
@@ -146,9 +158,106 @@ func main() {
 		setupLog.Info("PFC Error", "message", message)
 	}
 
+	// Clean up data from before we rebooted
+	ctx := context.Background()
+	if err := prebootCleanup(ctx); err != nil {
+		setupLog.Error(err, "problem running cleanup")
+		os.Exit(1)
+	}
+
 	setupLog.Info("starting manager")
 	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
 		setupLog.Error(err, "problem running manager")
 		os.Exit(1)
 	}
+}
+
+// prebootCleanup cleans out leftover data that might be
+// invalid. Ifindex values, for example, can change after a reboot so
+// we want to zero them and wait until Python re-writes them. PFC
+// tables should be initialized so we don't send packets to the wrong
+// place during startup.
+func prebootCleanup(ctx context.Context) error {
+
+	// Empty the PFC tables
+	err := exec.RunScript(setupLog, "/opt/acnodal/bin/pfc_cli_go initialize")
+	if err != nil {
+		return err
+	}
+
+	// We use an ad-hoc client here because the mgr.GetClient() doesn't
+	// start until you call mgr.Start() but we want to do this cleanup
+	// before the controllers start
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return err
+	}
+	cl, err := client.New(config, client.Options{
+		Scheme: scheme,
+	})
+	if err != nil {
+		return err
+	}
+
+	// List all of the LBs
+	list := egwv1.LoadBalancerList{}
+	if err = cl.List(ctx, &list); err != nil {
+		return err
+	}
+
+	// For each LB, wipe out the Status
+	for _, lb := range list.Items {
+		setupLog.Info("cleanup", "name", lb.Namespace+"/"+lb.Name, "status", lb.Status)
+
+		// Info relating to system network devices is unreliable at this
+		// point.
+		lb.Status.ProxyIfindex = 0
+		lb.Status.ProxyIfname = ""
+
+		// apply the update
+		if err = cl.Status().Update(ctx, &lb); err != nil {
+			setupLog.Info("updating LB status", "lb", lb, "error", err)
+		}
+	}
+
+	// "Nudge" the proxy pods to trigger the python daemon to re-populate the
+	// ifindex and veth fields
+	proxies, err := listProxyPods(ctx, cl)
+	if err != nil {
+		return err
+	}
+	for _, proxyPod := range proxies.Items {
+		setupLog.Info("nudge", "name", proxyPod.Namespace+"/"+proxyPod.Name)
+		nudgePod(ctx, cl, proxyPod)
+	}
+	return nil
+}
+
+// listProxyPods lists the pods that run our Envoy proxy.
+func listProxyPods(ctx context.Context, cl client.Client) (v1.PodList, error) {
+	listOps := client.ListOptions{
+		LabelSelector: labels.SelectorFromSet(map[string]string{"app": "egw", "role": "proxy"}),
+	}
+	list := v1.PodList{}
+	err := cl.List(ctx, &list, &listOps)
+
+	return list, err
+}
+
+// nudgePod "nudges" a pod, i.e., triggers an update event by
+// adding/modifying an annotation. This causes the setup-network
+// daemon to re-scan for the pod's veth and ifindex.
+func nudgePod(ctx context.Context, cl client.Client, pod v1.Pod) error {
+	if pod.Annotations == nil {
+		pod.Annotations = map[string]string{}
+	}
+	pod.Annotations["nudge"] = fmt.Sprintf("%x", time.Now().UnixNano())
+
+	// apply the update
+	if err := cl.Update(ctx, &pod); err != nil {
+		setupLog.Info("nudging pod", "pod", pod, "error", err)
+		return err
+	}
+
+	return nil
 }
