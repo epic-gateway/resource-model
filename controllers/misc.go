870863ace6d9fe8fed20ac6393e245c9517cb625
toby cabot <toby@acnodal.io>
2021-07-06 16:55:11 -0400

    Add functions to add and remove finalizers
    I'm amazed at how there doesn't seem to be a clear-cut best practice
    for something that's so critical to the operation of
    controllers. Here's my best guess at how to add and remove
    finalizers. I'd rather use Patch() than Update() but can't seem to
    find the right incantation.
    
    I copied this code from the eds-server and hope to be able to export
    it so we don't need to have it in multiple places.

--- controllers/misc.go
+++ controllers/misc.go
@@ -1,12 +1,78 @@
 package controllers
 
 import (
+	"context"
 	"time"
 
+	"k8s.io/client-go/util/retry"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
 
 var (
 	done     = ctrl.Result{Requeue: false}
 	tryAgain = ctrl.Result{RequeueAfter: 10 * time.Second}
 )
+
+// AddFinalizer safely adds "finalizerName" to the finalizers list of
+// "obj". See
+// https://pkg.go.dev/k8s.io/client-go/util/retry#RetryOnConflict for
+// more info.
+func AddFinalizer(ctx context.Context, cl client.Client, obj client.Object, finalizerName string) error {
+	if !controllerutil.ContainsFinalizer(obj, finalizerName) {
+		key := client.ObjectKey{Namespace: obj.GetNamespace(), Name: obj.GetName()}
+
+		err := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+			// Fetch the resource here; you need to refetch it on every try,
+			// since if you got a conflict on the last update attempt then
+			// you need to get the current version before making your own
+			// changes.
+			if err := cl.Get(ctx, key, obj); err != nil {
+				return err
+			}
+
+			// Add our finalizer
+			controllerutil.AddFinalizer(obj, finalizerName)
+
+			// Try to update
+			return cl.Update(ctx, obj)
+		})
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// RemoveFinalizer safely removes "finalizerName" from the finalizers
+// list of "obj". See
+// https://pkg.go.dev/k8s.io/client-go/util/retry#RetryOnConflict for
+// more info.
+func RemoveFinalizer(ctx context.Context, cl client.Client, obj client.Object, finalizerName string) error {
+	if controllerutil.ContainsFinalizer(obj, finalizerName) {
+		key := client.ObjectKey{Namespace: obj.GetNamespace(), Name: obj.GetName()}
+
+		err := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+			// Fetch the resource here; you need to refetch it on every try,
+			// since if you got a conflict on the last update attempt then
+			// you need to get the current version before making your own
+			// changes.
+			if err := cl.Get(ctx, key, obj); err != nil {
+				return err
+			}
+
+			// Remove our finalizer
+			controllerutil.RemoveFinalizer(obj, finalizerName)
+
+			// Try to update
+			return cl.Update(ctx, obj)
+		})
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
