933e1aef73f269124de299f2aef1ff1cd123c406
Toby Cabot <toby@acnodal.io>
2023-07-20 15:58:01 -0400

    Add a command that implements adhoc linux tunnels
    The adhoc-agent command sets up TrueIngress based on what it reads
    from EPIC.
    
    Signed-off-by: Toby Cabot <toby@acnodal.io>

---
+++ cmd/adhoc-agent.go
@@ -0,0 +1,96 @@
+package cmd
+
+import (
+	"net"
+
+	"github.com/spf13/cobra"
+	ctrl "sigs.k8s.io/controller-runtime"
+
+	"gitlab.com/acnodal/epic/resource-model/controllers"
+	"gitlab.com/acnodal/epic/resource-model/internal/exec"
+	"gitlab.com/acnodal/epic/resource-model/internal/network"
+	"gitlab.com/acnodal/epic/resource-model/internal/pfc"
+	// +kubebuilder:scaffold:imports
+)
+
+var (
+	ipAddress string
+	nicName   string
+
+	// adhocAgentCmd is the adhoc agent subcommand. An instance of the
+	// adhoc agent runs on each adhoc linux backend host and sets up the
+	// PFC tunnels on that host.
+	adhocAgentCmd = &cobra.Command{
+		Use:   "adhoc-agent",
+		Short: "Run the EPIC adhoc linux node agent",
+		RunE:  runAdhocAgent,
+	}
+)
+
+func init() {
+	var (
+		defIP  net.IP = net.ParseIP("10.0.0.1")
+		defNIC string = "eth0"
+	)
+
+	// Find the default outbound IP address.
+	if outboundIP, err := network.GetOutboundIP(); err == nil {
+		defIP = outboundIP
+		// Find which NIC has this IP.
+		if outboundNIC, err := network.GetInterfaceByIP(defIP); err == nil {
+			defNIC = outboundNIC
+		}
+	}
+	adhocAgentCmd.Flags().StringVarP(&ipAddress, "ip-address", "i", defIP.String(), "Tunnel IP address")
+	adhocAgentCmd.Flags().StringVarP(&nicName, "nic", "n", defNIC, "Tunnel network interface name")
+
+	rootCmd.AddCommand(adhocAgentCmd)
+}
+
+func runAdhocAgent(cmd *cobra.Command, args []string) error {
+	setupLog := ctrl.Log.WithName("setup")
+	setupLog.Info("Agent running")
+
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+		Scheme:             scheme,
+		MetricsBindAddress: "0",
+		LeaderElection:     false,
+	})
+	if err != nil {
+		return err
+	}
+
+	// Set up controllers
+	if err = (&controllers.GWProxyAdhocReconciler{
+		Client:        mgr.GetClient(),
+		RuntimeScheme: mgr.GetScheme(),
+		NodeAddress:   ipAddress,
+	}).SetupWithManager(mgr); err != nil {
+		return err
+	}
+
+	setupLog.V(1).Info("Cleaning up TrueIngress")
+
+	// Empty the PFC tables.
+	if err := exec.RunScript(setupLog, "/opt/acnodal/bin/pfc_cli_go initialize"); err != nil {
+		return err
+	}
+
+	setupLog.V(1).Info("Setting up TrueIngress")
+
+	// Hook TrueIngress into the network interface.
+	if err := pfc.SetupNIC(nicName, "decap", "ingress", 0, 9); err != nil {
+		setupLog.Error(err, "Failed to setup NIC "+nicName)
+	}
+	if err := pfc.SetupNIC(nicName, "encap", "egress", 1, 28); err != nil {
+		setupLog.Error(err, "Failed to setup NIC "+nicName)
+	}
+
+	setupLog.V(1).Info("starting agent")
+	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+		return err
+	}
+	setupLog.V(1).Info("agent returned, will exit")
+
+	return nil
+}
---
+++ controllers/gwproxy_adhoc_controller.go
@@ -0,0 +1,91 @@
+package controllers
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	epicv1 "gitlab.com/acnodal/epic/resource-model/api/v1"
+	epicexec "gitlab.com/acnodal/epic/resource-model/internal/exec"
+)
+
+// GWProxyAdhocReconciler reconciles a GWProxy object by performing
+// the per-node networking setup needed to enable True Ingress
+// tunnels.
+type GWProxyAdhocReconciler struct {
+	client.Client
+	RuntimeScheme *runtime.Scheme
+	NodeAddress   string
+}
+
+// +kubebuilder:rbac:groups=epic.acnodal.io,resources=gwproxies,verbs=get;list;watch;update;patch;delete
+// +kubebuilder:rbac:groups=epic.acnodal.io,resources=gwproxies/status,verbs=get;update;patch
+
+// Reconcile takes a Request and makes the system reflect what the
+// Request is asking for.
+func (r *GWProxyAdhocReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	l := log.FromContext(ctx).WithValues("node address", r.NodeAddress)
+	proxy := &epicv1.GWProxy{}
+
+	l.V(1).Info("Reconciling...")
+
+	// read the object that caused the event
+	if err := r.Get(ctx, req.NamespacedName, proxy); err != nil {
+		l.Info("can't get resource, probably deleted")
+		// ignore not-found errors, since they can't be fixed by an
+		// immediate requeue (we'll need to wait for a new notification),
+		// and we can get them on deleted requests.
+		return done, client.IgnoreNotFound(err)
+	}
+
+	// Set up each proxy pod belonging to this proxy
+	endpointMap, hasMap := proxy.Spec.GUETunnelMaps[r.NodeAddress]
+	if hasMap {
+		for _, tunnelInfo := range endpointMap.EPICEndpoints {
+			l.Info("setting up tunnel", "tunnel-id", tunnelInfo.TunnelID, "epicIP", tunnelInfo.Address, "port", tunnelInfo.Port.Port)
+
+			// Configure the tunnel between the EPIC node and this one.
+			if err := setTunnel(l, tunnelInfo.TunnelID, tunnelInfo.Address, r.NodeAddress, tunnelInfo.Port.Port); err != nil {
+				l.Error(err, "setting up tunnel", "epic-ip", os.Getenv("EPIC_HOST"), "client-info", tunnelInfo)
+			}
+
+			// Set up a service gateway from this proxy to this rep.
+			if err := setService(l, proxy.Spec.TunnelKey, tunnelInfo.TunnelID); err != nil {
+				l.Error(err, "setting up service gateway")
+			}
+
+		}
+	} else {
+		l.V(1).Info("No tunnels ending at this node")
+	}
+
+	return done, nil
+}
+
+// SetupWithManager sets up this controller to work with the mgr.
+func (r *GWProxyAdhocReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&epicv1.GWProxy{}).
+		Complete(r)
+}
+
+// Scheme returns this reconciler's scheme.
+func (r *GWProxyAdhocReconciler) Scheme() *runtime.Scheme {
+	return r.RuntimeScheme
+}
+
+// setTunnel sets the parameters needed by one PFC tunnel.
+func setTunnel(log logr.Logger, tunnelID uint32, tunnelAddr string, myAddr string, tunnelPort int32) error {
+	return epicexec.RunScript(log, fmt.Sprintf("/opt/acnodal/bin/cli_tunnel set %[1]d %[3]s %[4]d %[2]s %[4]d", tunnelID, tunnelAddr, myAddr, tunnelPort))
+}
+
+// setService sets the parameters needed by one PFC service.
+func setService(log logr.Logger, tunnelAuth string, tunnelID uint32) error {
+	return epicexec.RunScript(log, fmt.Sprintf("/opt/acnodal/bin/cli_service set-node %[1]d %[2]d %[3]s %[4]d", tunnelID>>16, tunnelID&0xff, tunnelAuth, tunnelID))
+}
